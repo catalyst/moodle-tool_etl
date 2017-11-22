bb22c45503be62dabb1c1d2a2165e204e61f4d26
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2017-11-23 10:41:59 +1100

    Add basic tests for base classes

---
+++ tests/source/source_base_test.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for source base class.
+ *
+ * @package    tool_etl
+ * @copyright  2017 Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use tool_etl\source\source_base;
+
+defined('MOODLE_INTERNAL') || die;
+
+class tool_etl_source_base_testcase extends advanced_testcase {
+    public function test_get_options() {
+        $actual = source_base::get_options();
+        $expected = array('source_ftp', 'source_sftp', 'source_sftp_key');
+        $this->assertEquals($expected, $actual);
+    }
+}
---
+++ tests/target/target_base_test.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for target base class.
+ *
+ * @package    tool_etl
+ * @copyright  2017 Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use tool_etl\target\target_base;
+
+defined('MOODLE_INTERNAL') || die;
+
+class tool_etl_target_base_testcase extends advanced_testcase {
+    public function test_get_options() {
+        $actual = target_base::get_options();
+        $expected = array('target_dataroot');
+        $this->assertEquals($expected, $actual);
+    }
+}
