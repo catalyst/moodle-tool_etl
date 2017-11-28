9109680495b29e89d9eea4b4eed21ec660b49ecd
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2017-11-28 11:35:41 +1100

    Fix tests for common_base

--- tests/common/common_base_test.php
+++ tests/common/common_base_test.php
@@ -60,7 +60,7 @@ class tool_etl_common_base_testcase extends advanced_testcase {
 
     public function data_provider_for_test_get_options() {
         return array(
-            array('source', array('source_ftp', 'source_sftp', 'source_sftp_key')),
+            array('source', array('source_ftp', 'source_sftp', 'source_sftp_key', 'source_folder')),
             array('target', array('target_dataroot')),
             array('processor', array('processor_default')),
         );
