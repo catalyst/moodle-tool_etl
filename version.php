debc3130c1957a5a597319f396ec046586c49f2b
Kateryna Degtyariova <katerynadegtyariova@catalyst-au.net>
2022-05-10 15:55:06 +1000

    Enable running grunt in CI

--- version.php
+++ version.php
@@ -27,3 +27,4 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2020012801; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015051101; // Requires this Moodle version.
 $plugin->component = 'tool_etl'; // Full name of the plugin (used for diagnostics).
+$plugin->supported  = [35, 310]; // A range of branch number of supported moodle versions.
